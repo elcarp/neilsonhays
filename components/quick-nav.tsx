a5906cbbc1633351a1b5e0c47559b67f3005e81e
elcarp <lise.carpenter@gmail.com>
2025-06-19 13:55:42 +0700

    update menu

--- components/quick-nav.tsx
+++ components/quick-nav.tsx
@@ -1,22 +1,23 @@
-import { BookHeart, Info, Mail, MapPin, UserRound } from "lucide-react"
+import { BookHeart, BookOpen, Calendar, Coffee, Info, Mail, MapPin, UserRound, Users2 } from "lucide-react"
 import Link from "next/link"
 
 const nav = [
-  { title: 'Kids', href: '#', icon: BookHeart },
-  { title: 'Membership', href: '#', icon: UserRound },
-  { title: 'Visit', href: '#', icon: MapPin },
-  { title: 'About', href: '#', icon: Info },
-  { title: 'Contact', href: '#', icon: Mail },
+  { title: 'Catalog', href: '#', icon: BookOpen },
+  { title: 'Events', href: '#', icon: Calendar },
+  { title: 'Kids Programs', href: '#', icon: BookHeart },
+  { title: 'Cafe Info', href: '#', icon: Coffee },
+  { title: 'Book Club', href: '#', icon: Users2 },
 ]
 
 export default function QuickNav() {
+
   return (
     <div className="flex gap-4 text-white text-center text-sm mt-10">
       {nav.map((item) => (
         <Link key={item.title} href={item.href} className="w-24 h-24 flex items-center justify-center">
           <div className="flex flex-col items-center justify-center">
             <div className="rounded-full bg-[#ffffff1a] hover:bg-[#ffffff] hover:text-teal-500 transition-all duration-300 w-12 h-12 flex items-center justify-center mb-2">
-              <BookHeart />
+              <item.icon />
             </div>
             <span>{item.title}</span>
           </div>
