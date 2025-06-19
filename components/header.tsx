1a54e6307809c0a65324657877030ca67914579b
elcarp <lise.carpenter@gmail.com>
2025-06-19 10:18:19 +0700

    clean up

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ import {
   Bars3Icon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
 import { BookHeart } from "lucide-react"
 import { Button } from "./ui/button"
