d8dcacce056ca4045b7145bb515f080fa98f4cbb
elcarp <lise.carpenter@gmail.com>
2025-06-24 09:22:07 +0700

    update logo title

--- components/header.tsx
+++ components/header.tsx
@@ -65,7 +65,7 @@ export default function Header() {
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">Neilson Hays Library</span>
             <Image
               src={isScrolled ? "/logo.svg" : "/logo-white.svg"}
               alt="Logo"
