65b769c7375550d47463df53e75b3489963e8cbc
elcarp <lise.carpenter@gmail.com>
2025-07-10 14:19:30 +0700

    resolve build error

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,19 @@
+import { Playfair_Display } from 'next/font/google'
+
+const playfair = Playfair_Display({
+  variable: '--font-playfair',
+  subsets: ['latin'],
+})
 export default function Events() {
-  return <div className="min-h-screen bg-teal-500 pt-35">
-    <div className="max-w-4xl px-8 mx-auto">
-      <h1 className="text-4xl font-bold text-white">Events</h1>
+  return (
+    <div className='min-h-screen bg-teal-500 pt-35'>
+      <div className='max-w-4xl px-8 mx-auto'>
+        <h1
+          className={`${playfair.className} text-4xl text-center font-bold text-white`}
+        >
+          Events
+        </h1>
+      </div>
     </div>
-  </div>
+  )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -17,6 +17,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
 import { BookHeart } from "lucide-react"
 import { Button } from "./ui/button"
+import Link from "next/link"
 
 const events = [
   {
@@ -64,7 +65,7 @@ export default function Header() {
       }`}>
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Neilson Hays Library</span>
             <Image
               src={isScrolled ? "/logo.svg" : "/logo-white.svg"}
@@ -73,7 +74,7 @@ export default function Header() {
               height={32}
               className="h-8 w-auto"
             />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -156,7 +157,7 @@ export default function Header() {
         <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Neilson Hays Library</span>
               <Image
                 src="/logo.svg"
@@ -165,7 +166,7 @@ export default function Header() {
                 height={32}
                 className="h-8 w-auto"
               />
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
