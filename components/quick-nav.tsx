5a1a5f74227c2486a18439be0352a107f7fcfc58
elcarp <lise.carpenter@gmail.com>
2025-09-04 20:06:04 +0700

    remove unused

--- components/quick-nav.tsx
+++ components/quick-nav.tsx
@@ -1,22 +1,35 @@
-import { BookHeart, BookOpen, Calendar, Coffee, Users2 } from "lucide-react"
-import Link from "next/link"
+import { BookHeart, BookOpen, Calendar, Coffee } from 'lucide-react'
+import Link from 'next/link'
 
 const nav = [
-  { title: 'Catalog', href: 'https://16309.rmwebopac.com/', target: '_blank', icon: BookOpen },
+  {
+    title: 'Catalog',
+    href: 'https://16309.rmwebopac.com/',
+    target: '_blank',
+    icon: BookOpen,
+  },
   { title: 'Events', href: '/events', icon: Calendar },
   { title: 'Kids Programs', href: '/kids', icon: BookHeart },
-  { title: 'Cafe Info', href: 'https://share.google/4nQBu34wtnSRrNWY5', icon: Coffee },
+  {
+    title: 'Cafe Info',
+    href: 'https://share.google/4nQBu34wtnSRrNWY5',
+    icon: Coffee,
+  },
   // { title: 'Book Club', href: '#', icon: Users2 },
 ]
 
 export default function QuickNav() {
-
   return (
-    <div className="flex gap-2 text-white text-center text-sm mt-10">
-      {nav.map((item) => (
-        <Link key={item.title} href={item.href} target={item.target ? item.target : '_self'} className="w-24 h-24 flex items-center justify-center">
-          <div className="flex flex-col items-center justify-center">
-            <div className="rounded-full bg-[#ffffff1a] hover:bg-[#ffffff] hover:text-teal-500 transition-all duration-300 w-12 h-12 flex items-center justify-center mb-2">
+    <div className='flex gap-2 text-white text-center text-sm mt-10'>
+      {nav.map(item => (
+        <Link
+          key={item.title}
+          href={item.href}
+          target={item.target ? item.target : '_self'}
+          className='w-24 h-24 flex items-center justify-center'
+        >
+          <div className='flex flex-col items-center justify-center'>
+            <div className='rounded-full bg-[#ffffff1a] hover:bg-[#ffffff] hover:text-teal-500 transition-all duration-300 w-12 h-12 flex items-center justify-center mb-2'>
               <item.icon />
             </div>
             <span>{item.title}</span>
